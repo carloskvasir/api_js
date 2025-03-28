01941536b9535e42a6019e04e61869fe1f6698eb
Carlos Kvasir <git@carloskvasir.dev>
2025-03-28 17:20:37 -0300

    feat: add basic `/hello` endpoint with database version check
    - Implemented a `/hello` route to return a welcome message and database version.
    - Added PostgreSQL client configuration for database connection.
    - Updated dependencies in `package.json`.

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const express = require('express');
+const { Client } = require('pg');
+
 const app = express();
 const PORT = 3000;
 
-// Middleware to parse JSON
+// Middleware para parsear JSON
 app.use(express.json());
 
-// Root route
-app.get('/hello', (req, res) => {
-  res.json({
-    message: "Welcome to @caloskvasir's Express API!"
-  });
+// Configurações do banco de dados
+const dbConfig = {
+  user: 'db_user',
+  host: 'localhost',
+  database: 'db_name',
+  password: 'db_pass',
+  port: 5432,
+};
+
+// Rota raiz
+app.get('/hello', async (req, res) => {
+  const client = new Client(dbConfig);
+
+  try {
+    await client.connect();
+    const result = await client.query('SELECT version();');
+    const dbVersion = result.rows[0].version;
+    res.json({
+      message: "Welcome to @caloskvasir Express API!",
+      db_version: dbVersion,
+    });
+  } catch (err) {
+    console.error('Error when connecting:', err);
+    res.status(500).json({ error: 'Intern error' });
+  } finally {
+    await client.end();
+  }
 });
 
-// Start the server
+// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
-
